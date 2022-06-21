45c82b06442b97639c95ea6bd8275807ce825d69
Jaro Dev <33616710+jarodev@users.noreply.github.com>
2022-06-21 20:26:03 +0200

    Include return statements

--- apps/aa-1x1/tasks.js
+++ apps/aa-1x1/tasks.js
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import axios from 'axios';
+import Typography from '@mui/material/Typography';
 
 export default function Tasks(props) {
 	const body = {
 		numberOfTasks: String(props.count),
 		itemsToScrumble: props.summands,
 	};
-	const output = getTasks(body);
+	let output;
+	output = getTasks(body);
+	console.log(output);
 
-	return <p>{JSON.stringify(output)}</p>;
+	output.map((task) => {
+		const returnString = `${task.item1} * ${task.item2} = `;
+		console.log(returnString);
+		return (
+			<Typography variant="body1" key={`${task.item1}_${task.item2}`}>
+				{returnString}
+			</Typography>
+		);
+	});
 }
 
 function getTasks(body) {
@@ -20,10 +31,15 @@ function getTasks(body) {
 
 	const url = 'http://localhost:3000/api/aa-1x1';
 
+	const tasksToReturn = [];
+
 	axios
 		.post(url, body, options)
-		.then((res) => console.log(`success`))
+		.then((res) => {
+			//return JSON.stringify(res.data);
+			res.data.tasks.map((task) => tasksToReturn.push(task));
+		})
 		.catch((e) => console.log(`error: ${e}`));
 
-	return '';
+	return tasksToReturn;
 }
