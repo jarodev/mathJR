50d44425e577250e24b15ff2a56edcd01063e25b
Jaro Dev <33616710+jarodev@users.noreply.github.com>
2022-08-22 21:06:36 +0200

    updates

--- utils/supabaseAdminClient.js
+++ utils/supabaseAdminClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseServiceRoleKey =
+	process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY;
+
 export const supabaseAdminClient = createClient(
-	process.env.NEXT_PUBLIC_SUPABASE_URL,
-	process.env.SUPABASE_SERVICE_ROLE_KEY
+	supabaseUrl,
+	supabaseServiceRoleKey
 );
